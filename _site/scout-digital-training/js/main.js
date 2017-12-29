92f34fab0f2bf7e094742a0c1613bf0abc4f2817
enistico <erica@scoutdm.com.au>
2017-12-29 15:04:10 +1030

    Active Campaign subscribe form added to homepage, link to LinkedIn changed
    LinkedIn now links to Scout Digital Training LinkedIn company page

---
+++ _site/scout-digital-training/js/main.js
@@ -0,0 +1,89 @@
+$(document).ready( function () {
+
+// Full Calendar
+$('#calendar').fullCalendar({
+        // put your options and callbacks here
+
+        eventSources: [
+
+        // your event source
+        {
+            events: [ // put the array in the `events` property
+                {
+                    title  : 'Introduction to Google Analytics',
+                    start  : '2017-04-20',
+                    url    : '/google-analytics-intro.html',
+                },
+                {
+                    title  : 'Taking Google Analytics Further',
+                    start  : '2017-05-11',
+                    url    : '/google-analytics-further.html',
+                },
+                {
+                    title  : 'Introduction to SEO Best Practices',
+                    start  : '2017-06-08',
+                    url    : '/seo-basics.html',
+                },
+                {
+                    title  : 'Introduction to Google Analytics',
+                    start  : '2017-07-06',
+                    url    : '/google-analytics-intro.html',
+                },
+                {
+                    title  : 'Taking Google Analytics Further',
+                    start  : '2017-08-03',
+                    url    : '/google-analytics-further.html',
+                },
+                {
+                    title  : 'Introduction to SEO Best Practices',
+                    start  : '2017-08-31',
+                    url    : '/seo-basics.html',
+                },
+                {
+                    title  : 'Introduction to Google Analytics',
+                    start  : '2017-09-28',
+                    url    : '/google-analytics-intro.html',
+                },
+                {
+                    title  : 'Taking Google Analytics Further',
+                    start  : '2017-10-26',
+                    url    : '/google-analytics-further.html',
+                },
+                {
+                    title  : 'Introduction to SEO Best Practices',
+                    start  : '2017-11-23',
+                    url    : '/seo-basics.html',
+                }
+            ],
+            color: 'orange',     // an option!
+            textColor: 'black' // an option!
+        }
+
+        // any other event sources...
+
+    ]
+
+    })
+
+// Owl Carousel
+// basic demo, responsive with next/prev nav
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+
+});
+
+// END OF DOCUMENT READY
